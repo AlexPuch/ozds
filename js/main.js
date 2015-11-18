483546e76e27bf7f5675436790c29f3bab69aebb
Alex Puch <oleksandr.puchkov@gmail.com>
2015-11-19 00:44:19 +0200

    bootstrap edition

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 /*ховер поста на главной*/
+/*
 $(document).on('mouseenter','.project-description', function(){
   $(this).css("opacity","1");
 });
@@ -7,11 +8,13 @@ $(document).on('mouseleave','.project-description', function(){
   $(this).css("opacity","0");
 });
 /*ховер меню*/
+/*
 $(document).on('click','li.burger', function(){
   $(this).toggleClass("show");
   $(".main-menu, .close, .lang").toggleClass("show");
   $(".main-menu").hasClass("show") ? $(".navbar-inner").addClass("big-nav") : $(".navbar-inner").removeClass("big-nav");
 });
+*/
 //Google maps
 var map;
 var marker;
