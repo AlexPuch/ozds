8acd5bb43c7b07af0109c857133df47d0e301be3
Alex Puch <oleksandr.puchkov@gmail.com>
2015-11-19 17:52:19 +0200

    add filter fix menu
    не плавно срабаті

--- js/main.js
+++ js/main.js
@@ -1,20 +1,14 @@
-/*ховер поста на главной*/
-/*
-$(document).on('mouseenter','.project-description', function(){
-  $(this).css("opacity","1");
+//collapse
+$(document).on("show.bs.collapse","#myNavbar", function() {
+  $(".lang").removeClass("show");
+  $(".burger").html('<span class="glyphicon glyphicon-remove"></span>');
+});
+$(document).on("hide.bs.collapse","#myNavbar", function() {
+  $(".lang").addClass("show");
+  $(".burger").html('<span class="glyphicon glyphicon-menu-humburger"></span>');
 });
 
-$(document).on('mouseleave','.project-description', function(){
-  $(this).css("opacity","0");
-});
-/*ховер меню*/
-/*
-$(document).on('click','li.burger', function(){
-  $(this).toggleClass("show");
-  $(".main-menu, .close, .lang").toggleClass("show");
-  $(".main-menu").hasClass("show") ? $(".navbar-inner").addClass("big-nav") : $(".navbar-inner").removeClass("big-nav");
-});
-*/
+
 //Google maps
 var map;
 var marker;
@@ -190,5 +184,5 @@ function toggleBounce() {
   } else {
     marker.setAnimation(google.maps.Animation.BOUNCE);
   }
-},();
+};
 
